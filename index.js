0d470c605989650b5c69e64fc66cacfe52805a08
Natanel Mordov <137004361+NatanelMordov@users.noreply.github.com>
2024-02-11 12:37:24 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+/* Scroll reveal animation */
+const sr= ScrollReveal({
+  origin:'top',
+  distance:'60px',
+  duration:2500,
+  delay:400,
+  //reset:true
+})
+
+sr.reveal('#profile, .contact_info_up_conteiner', {origin:'right'})
+sr.reveal('.section, .nav-links', {origin:'left'})
+sr.reveal('.experience_diteils_conteiner, .section_text_p2, .title', {interval:200})
